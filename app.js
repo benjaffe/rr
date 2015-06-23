38178e4ec704d034d580993876f7dd2c6ef2a565
Ben Jaffe <ben@udacity.com>
2015-06-23 14:09:53 -0700

    refactor page actions

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 var rr = rr || {};
 
 (function(app) {
+  // Our ViewModel
+  var vm = app.vm || {};
+  app.vm = vm;
+
   // Initial data
-  var vm;
   var model = {
     name: 'Loading',
     children: [],
@@ -19,8 +22,6 @@ var rr = rr || {};
 
   app.storage.setRRData(new app.model.Page(model, undefined));
 
-  // Our ViewModel
-  app.vm = vm = {};
 
   vm.getUrl = function(page) {
     return '#/' + app.router.getUrlToPage(page);
@@ -98,85 +99,20 @@ var rr = rr || {};
 
     // add an intro video to the page action queue
     if (page.introVideo) {
-      app.pageActions.addAction({
-        run: function(cleanup) {
-          var self = this;
-          // show the modal
-          var modal = $('#video-modal').modal('show');
-
-          // initialize a youtube player
-          app.youtubePlayer = new YT.Player('youtube-player', {
-            height: '390',
-            width: '640',
-            videoId: vm.currentPage().introVideo.src(),
-            playerVars: {
-              autoplay: true,
-              // controls: 0,
-              rel: 0,
-              showinfo: 0
-            },
-            events: {
-              'onStateChange': onPlayerStateChange
-            }
-          });
-
-          // When video ends, finish the page action
-          function onPlayerStateChange(event) {
-            if (event.data === YT.PlayerState.ENDED) {
-              self.cleanup();
-            }
-          }
-
-          // when modal is hidden, destroy the video and finish the page action.
-          // (Since the modal is closed already, pass in parameters to tell our
-          // cleanup function to skip that functionality)
-          modal.on('hidden.bs.modal', function(e) {
-            app.youtubePlayer.destroy();
-            self.cleanup({modal: false});
-          });
-        },
-
-        cleanup: function(options) {
-          if (!(options && options.modal === false)) {
-            // remove the backdrop manually, since bootstrap keeps it around
-            // when a new modal opens during the removal of the previous one
-            $('.modal-backdrop').fadeOut(function() {
-              this.remove();
-            });
-            $('#video-modal').modal('hide');
-          }
-        },
+      app.pageActions.addAction('VideoAction', {
+        videoId: vm.currentPage().introVideo.src()
       });
     }
 
     // add a navigate-to-resource to the page action queue
     if (page.navigateTo) {
-      app.pageActions.addAction({
-        run: function(cleanup) {
-          var self = this;
-          console.log('running', this);
-          // ------
+      app.pageActions.addAction('NavigateAction', {
 
-          var modal = $('#iframe-modal').modal('show');
-          modal.on('hidden.bs.modal', function(e) {
-            self.cleanup({modal: false});
-          });
-        },
-        cleanup: function(options) {
-          if (!(options && options.modal === false)) {
-            // remove the backdrop manually, since bootstrap keeps it around
-            // when a new modal opens during the removal of the previous one
-            $('.modal-backdrop').fadeOut(function() {
-              this.remove();
-            });
-            $('#video-modal').modal('hide');
-          }
-
-          app.vm.currentPage().pageState.set({linkVisited: true});
-        }
       });
     }
+
     app.pageActions.startActions();
+
   });
 
   // when youtube api is ready, take us one step closer to initializing the app
--- js/page-actions.js
+++ js/page-actions.js
@@ -1,6 +1,9 @@
 var rr = rr || {};
 
 (function(app) {
+  var vm = app.vm || {};
+  app.vm = vm;
+  vm.currentActionName = ko.observable('');
 
   var actionQueue = [];
   var currentAction = null;
@@ -9,13 +12,108 @@ var rr = rr || {};
   var pageActions = app.pageActions;
   var i = 0;
 
-  function PageAction(options) {
-    if (!options.run || !options.cleanup) {
-      throw new Error('PageAction must have a run and cleanup function');
+  var Action = {
+    run: function() {
+      console.debug('running ' + this.type, this);
+      vm.currentActionName(this.type);
+      this._run.apply(this, arguments);
+    },
+    cleanup: function() {
+      if (this.hasRun) {
+        return false;
+      }
+      this.hasRun = true;
+
+      console.debug('cleaning up ' + this.type, this);
+
+      this._cleanup.apply(currentAction, arguments);
+      vm.currentActionName('');
+      markActionAsDoneAndContinue();
     }
-    options.uid = i++;
-    $.extend(this, options);
-  }
+  };
+
+  var actions = {};
+
+  // custom actions have no default _run and _cleanup functions
+  actions.CustomAction = $.extend(Object.create(Action), {});
+
+  actions.NavigateAction = $.extend(Object.create(Action), {
+    name: 'NavigateAction',
+    _run: function(cleanup) {
+      var self = this;
+      console.log('NAVIGATING', this, this.options);
+      // ------
+
+      var modal = $('#iframe-modal').modal('show');
+      console.log('yo', $('#iframe-modal'));
+      modal.on('hidden.bs.modal', function(e) {
+        self.cleanup({modal: false});
+      });
+    },
+    _cleanup: function(options) {
+      if (!(options && options.modal === false)) {
+        // remove the backdrop manually, since bootstrap keeps it around
+        // when a new modal opens during the removal of the previous one
+        $('.modal-backdrop').fadeOut(function() {
+          this.remove();
+        });
+        $('#video-modal').modal('hide');
+      }
+
+      app.vm.currentPage().pageState.set({linkVisited: true});
+    }
+  });
+
+  actions.VideoAction = $.extend(Object.create(Action), {
+    name: 'VideoAction',
+    _run: function(cleanup) {
+      var self = this;
+      // show the modal
+      var modal = $('#video-modal').modal('show');
+
+      // initialize a youtube player
+      app.youtubePlayer = new YT.Player('youtube-player', {
+        height: '390',
+        width: '640',
+        videoId: this.options.videoId,
+        playerVars: {
+          autoplay: true,
+          // controls: 0,
+          rel: 0,
+          showinfo: 0
+        },
+        events: {
+          'onStateChange': onPlayerStateChange
+        }
+      });
+
+      // When video ends, finish the page action
+      function onPlayerStateChange(event) {
+        if (event.data === YT.PlayerState.ENDED) {
+          self.cleanup();
+        }
+      }
+
+      // when modal is hidden, destroy the video and finish the page action.
+      // (Since the modal is closed already, pass in parameters to tell our
+      // cleanup function to skip that functionality)
+      modal.on('hidden.bs.modal', function(e) {
+        app.youtubePlayer.destroy();
+        self.cleanup({modal: false});
+      });
+    },
+
+    _cleanup: function(options) {
+      if (!(options && options.modal === false)) {
+        // remove the backdrop manually, since bootstrap keeps it around
+        // when a new modal opens during the removal of the previous one
+        $('.modal-backdrop').fadeOut(function() {
+          this.remove();
+        });
+        $('#video-modal').modal('hide');
+      }
+    }
+  });
 
   pageActions.cleanupAndStop = function() {
     actionQueue = [];
@@ -24,22 +122,24 @@ var rr = rr || {};
     }
   };
 
-  pageActions.addAction = function(options) {
-    var originalCleanup = options.cleanup;
-    options.cleanup = function() {
-      if (this.hasRun) {
-        return false;
-      }
-      this.hasRun = true;
+  pageActions.addAction = function(type, options) {
+    var Action = actions[type] || actions.custom;
+    var action = Object.create(Action);
+    console.log(action);
 
-      originalCleanup.apply(currentAction, arguments);
-      markActionAsDone();
-    };
+    // ship the options with the page action
+    action.options = options;
+    action.type = type;
 
-    actionQueue.push(new PageAction(options));
+    if (!action.run || !action.cleanup) {
+      throw new Error('PageAction must have a run and cleanup function');
+    }
+    action.uid = i++;
+
+    actionQueue.push(action);
   };
 
-  function markActionAsDone() {
+  function markActionAsDoneAndContinue() {
     currentAction = null;
     pageActions.startActions();
   }
